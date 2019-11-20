9f20f38fefdc2dc263831c7a75db824f00f5976b
Ronald Hristov <ronald@azonmedia.com>
2019-11-20 11:47:41 +0200

    Added YML RegistryBackend with an example file

--- src/RegistryBackendYml.php
+++ src/RegistryBackendYml.php
@@ -5,6 +5,9 @@ namespace Azonmedia\Registry;
 
 
 use Azonmedia\Registry\Interfaces\RegistryBackendInterface;
+use DirectoryIterator;
+use Exception;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * Class RegistryBackendYml
@@ -18,8 +21,42 @@ use Azonmedia\Registry\Interfaces\RegistryBackendInterface;
  * With implied value of string
  * @package Azonmedia\Registry
  */
-class RegistryBackendYml
-implements RegistryBackendInterface
+class RegistryBackendYml extends RegistryBackendArray implements RegistryBackendInterface
 {
+    /**
+     * Global config files can be included in git
+     * Local config files (*.local.yaml) contain environment specific data and should be in the gitignore
+     * Local files override global config files
+     *
+     * @param string $config_path
+     * @throws Exception
+     */
+    public function generate_config(string $config_path)
+    {
+        if (!is_dir($config_path)) {
+            throw new Exception(sprintf('Config path %s does not exist', $config_path));
+        }
 
+        foreach (new DirectoryIterator($config_path) as $file) {
+            if ($file->isDot()) {
+                continue;
+            }
+
+            if ($file->isFile()) {
+                if (!in_array($file->getExtension(), ['yaml', 'yml'])) {
+                    continue;
+                }
+
+                if (substr($file->getFilename(), -10) === '.local.yml' || substr($file->getFilename(), -11) === '.local.yaml' || in_array($file->getFilename(), ['local.yml', 'local.yaml'])) {
+                    $current_config = Yaml::parseFile($file->getRealPath());
+                    $this->local_config = array_replace_recursive($this->local_config, $current_config);
+                } else {
+                    $current_config = Yaml::parseFile($file->getRealPath());
+                    $this->global_config = array_replace_recursive($this->global_config, $current_config);
+                }
+            } elseif ($file->isDir()) {
+                $this->generate_config($file->getRealPath());
+            }
+        }
+    }
 }
